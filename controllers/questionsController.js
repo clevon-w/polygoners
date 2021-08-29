04063d68c91d6c16bb640ae7c6cb4ad030676f54
joeltancej <joeltancej@gmail.com>
2021-08-30 00:23:34 +0800

    Added all qns

--- controllers/questionsController.js
+++ controllers/questionsController.js
@@ -106,9 +106,207 @@ exports.questionsController = (req, res) => {
       },
       //Health Qns
       {
-        question: "this is the first health question",
+        question: "I get 7-8 hours of sleep everyday.",
         type: "health",
-        id: 2
+        id: 20
+      },
+      {
+        question: "I cope with mental fatigue and stress very well.",
+        type: "health",
+        id: 21
+      },
+      {
+        question: "I follow my fitness program strictly.",
+        type: "health",
+        id: 22
+      },
+      {
+        question: "I am very conscientious of my diet.",
+        type: "health",
+        id: 23
+      },
+      {
+        question: "I spend a lot of time on self-care and ensuring my mental stability.",
+        type: "health",
+        id: 24
+      },
+      {
+        question: "To me, having a good work-life balance is of utmost importance.",
+        type: "health",
+        id: 25
+      },
+      {
+        question: "After a long social activity, I need to take time to recover and recharge my energy alone.",
+        type: "health",
+        id: 26
+      },
+      {
+        question: "I am rarely stressed or frustrated when things do not go my way.",
+        type: "health",
+        id: 27
+      },
+      {
+        question: "I dedicate a lot of time to my hobbies and interests to attempt to convert them into proficiencies.",
+        type: "health",
+        id: 28
+      },
+      {
+        question: "Even during busy periods, I will find ways to make time for my hobbies and interests.",
+        type: "health",
+        id: 29
+      },
+      //RS Qns
+      {
+        question: "I open jio supper or attend open jios often.",
+        type: "relationship",
+        id: 30
+      },
+      {
+        question: "I enjoy meeting new people and take the initiative to make new friends.",
+        type: "relationship",
+        id: 31
+      },
+      {
+        question: "I believe that finding a romantic partner in university is essential.",
+        type: "relationship",
+        id: 32
+      },
+      {
+        question: "I actively set aside time to spend with my family.",
+        type: "relationship",
+        id: 33
+      },
+      {
+        question: "I catch up with my non-university friends on a regular basis.",
+        type: "relationship",
+        id: 34
+      },
+      {
+        question: "I enjoy attending drinking sessions or parties.",
+        type: "relationship",
+        id: 35
+      },
+      {
+        question: "I believe a fulfilling university life is largely dependent on the friends that you make and relationships that you forge.",
+        type: "relationship",
+        id: 36
+      },
+      {
+        question: "I regain energy from spending time with my friends and loved ones.",
+        type: "relationship",
+        id: 37
+      },
+      {
+        question: "I choose to stay on campus to make more friends, have fun and have a more fulfilling university life.",
+        type: "relationship",
+        id: 38
+      },
+      {
+        question: "I am willing to set aside my plans to sit down, listen and help my friends or family members with their problems.",
+        type: "relationship",
+        id: 39
+      },
+      //Academic Qns
+      {
+        question: "I feel that my GPA significantly affects my future.",
+        type: "academic",
+        id: 40
+      },
+      {
+        question: "I believe a double major, minor or double degree will help me a lot of with job sourcing.",
+        type: "academic",
+        id: 41
+      },
+      {
+        question: "Rather than thinking of using up all my S/Us, I would rather devote time to every module and aim to pull up and stabilise my cap.",
+        type: "academic",
+        id: 42
+      },
+      {
+        question: "I make sure that I am managing my academic studies well before meeting my friends or picking up new activities.",
+        type: "academic",
+        id: 43
+      },
+      {
+        question: "If I feel that my style of studying isn't working, I will devise a new one.",
+        type: "academic",
+        id: 44
+      },
+      {
+        question: "I place a lot of pressure on myself to succeed academically.",
+        type: "academic",
+        id: 45
+      },
+      {
+        question: "I aim to perform above average for every module.",
+        type: "academic",
+        id: 46
+      },
+      {
+        question: " I follow my timetable very strictly to ensure that I have sufficient time to study.",
+        type: "academic",
+        id: 47
+      },
+      {
+        question: "I will never sacrifice the time I have allocated for studying to compensate for a lack of sleep. ",
+        type: "academic",
+        id: 48
+      },
+      {
+        question: "To me, studying is not an option, it is a neccessity for a better future.",
+        type: "academic",
+        id: 49
+      },
+      //Hustle Qns
+      {
+        question: "I regularly work part time to earn additional income, or to cover for my own allowance.",
+        type: "hustle",
+        id: 50
+      },
+      {
+        question: "I try to earn as much money as I can, even when I am busy with my university studies.",
+        type: "hustle",
+        id: 51
+      },
+      {
+        question: "I actively come up with new project or business ideas to make good use of my time in university.",
+        type: "hustle",
+        id: 52
+      },
+      {
+        question: "I feel that the skillsets I learn from part-time jobs, even though unrelated to my degree, are extremely valuable to prepare me for the future.",
+        type: "hustle",
+        id: 53
+      },
+      {
+        question: "My current goal is to establish a self-sustaining business and achieve financial security at a young age.",
+        type: "hustle",
+        id: 54
+      },
+      {
+        question: "I am trying to turn my interests and skills into chargeable services for the public.",
+        type: "hustle",
+        id: 55
+      },
+      {
+        question: "I am currently involved in non-school related projects aimed to generate income and add value to society.",
+        type: "hustle",
+        id: 56
+      },
+      {
+        question: "I am currently doing investing with my excess savings.",
+        type: "hustle",
+        id: 57
+      },
+      {
+        question: "I actively seek to leverage on the skills of the people around me to create business projects that can potentially have good returns.",
+        type: "hustle",
+        id: 58
+      },
+      {
+        question: "I believe a side hustle is crucial to having a growth mindset.",
+        type: "hustle",
+        id: 59
       },
     ]
   })
