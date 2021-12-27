70716bb0df0577db9d439a0c5ed43954185712b6
clevon-w <e0726468@u.nus.edu>
2021-12-27 21:35:06 +0800

    half of submit button functionality

--- frontend/src/Questions.js
+++ frontend/src/Questions.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {Grid, Typography, Card, CardContent, Box} from '@material-ui/core';
+import {Grid, Typography, Card, CardContent, Box, Radio, RadioGroup, FormControlLabel, FormControl, FormLabel, Button} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import background from './img/homepage_bg_img.jpeg';
 
@@ -38,6 +38,84 @@ function Questions() {
     }).then(jsonRes => setQuestions(jsonRes.questionsJson))
   })
 
+  const initialFormData = Object.freeze({
+    cca0: "",
+    cca1: "",
+    cca2: "",
+    cca3: "",
+    cca4: "",
+    cca5: "",
+    cca6: "",
+    cca7: "",
+    cca8: "",
+    cca9: "",
+    exp10: "",
+    exp11: "",
+    exp12: "",
+    exp13: "",
+    exp14: "",
+    exp15: "",
+    exp16: "",
+    exp17: "",
+    exp18: "",
+    exp19: "",
+    health20: "",
+    health21: "",
+    health22: "",
+    health23: "",
+    health24: "",
+    health25: "",
+    health26: "",
+    health27: "",
+    health28: "",
+    health29: "",
+    relationship30: "",
+    relationship31: "",
+    relationship32: "",
+    relationship33: "",
+    relationship34: "",
+    relationship35: "",
+    relationship36: "",
+    relationship37: "",
+    relationship38: "",
+    relationship39: "",
+    academic40: "",
+    academic41: "",
+    academic42: "",
+    academic43: "",
+    academic44: "",
+    academic45: "",
+    academic46: "",
+    academic47: "",
+    academic48: "",
+    academic49: "",
+    hustle50: "",
+    hustle51: "",
+    hustle52: "",
+    hustle53: "",
+    hustle54: "",
+    hustle55: "",
+    hustle56: "",
+    hustle57: "",
+    hustle58: "",
+    hustle59: ""
+  });
+
+  const [formData, updateFormData] = React.useState(initialFormData);
+
+  const handleChange = (e) => {
+    updateFormData({
+      ...formData, // spread syntax which allows an iterable to be expanded whenever placed
+      [e.target.name]: e.target.value.trim()
+    });
+  };
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(formData);
+    // ..code to submit form to backend here...
+  }
+
   return (
     <div>
       <Grid container direction="row" justifyContent="center" alignItems="center">
@@ -71,17 +149,31 @@ function Questions() {
         </Grid>
         <Grid item xs={12} md={6}>
           <Typography variant="h4">Questions page</Typography>
-          <div>
+          <form onSubmit={handleSubmit}> {/* instead of a div here should be a form with action as where in the backend u want to send it to as well as the method=post */}
             {questions.map(question => 
-              <ul>
-                <li>{question.question}</li>
-                <ul>
-                  <li>{question.type}</li>
-                  <li>{question.id}</li>
-                </ul>
-              </ul>
+              <FormControl component="fieldset" key={question.type + question.id} >
+                <FormLabel component="legend">{question.question}</FormLabel>
+                <RadioGroup aria-label="question_score" name={question.type + question.id} row style={{display: 'block'}}>
+                  <FormControlLabel value="1" control={<Radio />} onChange={handleChange} />
+                  <FormControlLabel value="2" control={<Radio />} onChange={handleChange} />
+                  <FormControlLabel value="3" control={<Radio />} onChange={handleChange} />
+                  <FormControlLabel value="4" control={<Radio />} onChange={handleChange} />
+                  <FormControlLabel value="5" control={<Radio />} onChange={handleChange} />
+                </RadioGroup>
+              </FormControl>
             )}
-          </div>
+            {/* <FormControl component="fieldset">
+              <FormLabel component="legend">DUMMY QUESTION</FormLabel>
+              <RadioGroup aria-label="question_score" name="SOMENAME" row style={{display: 'block'}}>
+                <FormControlLabel value="1" control={<Radio />} />
+                <FormControlLabel value="2" control={<Radio />} />
+                <FormControlLabel value="3" control={<Radio />} />
+                <FormControlLabel value="4" control={<Radio />} />
+                <FormControlLabel value="5" control={<Radio />} />
+              </RadioGroup>
+            </FormControl> */}
+            <Button type="submit">Submit</Button>
+          </form>
         </Grid>
       </Grid>
     </div>
